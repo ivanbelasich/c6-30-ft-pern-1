6aa152fdc8777cb3fe189dac61ee49ba005b991b
Joe <jb.barrientos@yahoo.com>
2022-08-09 17:24:18 -0300

    Working validation and authentication login

--- auth/routes/login/loginAuthenticator.js
+++ auth/routes/login/loginAuthenticator.js
@@ -1,4 +1,10 @@
-function loginAuthenticator(user, password) {
-    return false
+const User = require("../../sequelize/models/user")
+const recodePassword = require("../../utils/recodePassword")
+
+async function loginAuthenticator(inputUser, inputPassword) {
+    let userQuery = await User.findOne({ where: { user: inputUser } })
+    if (!userQuery) return false
+    let { password, salt } = userQuery.toJSON()
+    return recodePassword(inputPassword, salt) === password
 }
 module.exports = loginAuthenticator
