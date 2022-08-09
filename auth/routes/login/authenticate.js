dd92bbb89337ab0f9a177f71ce6081c554726524
Joe <jb.barrientos@yahoo.com>
2022-08-09 18:33:14 -0300

    Bugfix: Upon an invalid username/password, server should respond with code 400 - bad request

--- auth/routes/login/authenticate.js
+++ auth/routes/login/authenticate.js
@@ -3,7 +3,7 @@ function authenticate(validator, authenticator, tokenGenerator, authenticationEr
         let { user, password } = req.body
 
         const isValid = validator(user, password)
-        if (!isValid) return res.send(authenticationError("Invalid username or password"))
+        if (!isValid) return res.status(400).send(authenticationError("Invalid username or password"))
 
         const isAuthentic = await authenticator(user, password)
         if (!isAuthentic) return res.status(403).send(authenticationError("Wrong username or password"))
