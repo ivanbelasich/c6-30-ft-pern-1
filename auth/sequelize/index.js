a3327a5a745231765574e45d8bc95482b8a8872e
Joe <jb.barrientos@yahoo.com>
2022-08-09 04:53:17 -0300

    Adding sequelize-cli boilerplate for database seeding

--- auth/sequelize/index.js
+++ auth/sequelize/index.js
@@ -1,4 +1,13 @@
 const { Sequelize } = require("sequelize")
-const sequelize = new Sequelize(process.env.AUTH_DB)
+
+const sequelize = new Sequelize(
+    process.env.AUTH_DB_NAME, 
+    process.env.AUTH_DB_USER,
+    process.env.AUTH_DB_PASSWORD, {
+        host: process.env.AUTH_DB_HOST,
+        port: process.env.AUTH_DB_PORT,
+        dialect: "postgres"
+    } )
+
 
 module.exports = sequelize
