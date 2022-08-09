16b0793ac21155d5c2d6f34de08ce333341dea5b
Joe <jb.barrientos@yahoo.com>
2022-08-09 16:03:28 -0300

    Bugfix: Adding path configuration to dotenv, avoiding errors when initializing outside the auth folder

--- auth/app.js
+++ auth/app.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config({ path: path.join(__dirname, '.env') })
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login')
