be26a2cb49c3df6e20d5d8b8933ab94ad52d1605
Joe <jb.barrientos@yahoo.com>
2022-08-08 21:59:49 -0300

    Feat: Adding validation logic to login inputs

--- auth/routes/login/loginValidator.js
+++ auth/routes/login/loginValidator.js
@@ -1,4 +1,9 @@
-function loginValidator(user, password){
+function loginValidator(user, password) {
+    let checkTypes = { user, password }
+    for (let field in checkTypes) {
+        if (typeof checkTypes[field] !== "string") return false
+        if (checkTypes[field].length < 1) return false
+    }
     return true
 }
 
