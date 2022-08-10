6d8994386126f8212bb032c644b61c21a90d5865
Joe <jb.barrientos@yahoo.com>
2022-08-10 19:13:50 -0300

    Bugfix: Replaced environment variable names with correct ones

--- auth/jwt/tokenInfo.js
+++ auth/jwt/tokenInfo.js
@@ -1,8 +1,8 @@
 const tokenInfo = {
-    accessTokenValidityDays: parseInt(process.env.ACCESS_TOKEN_VALIDITY_DAYS || '0'),
-    refreshTokenValidityDays: parseInt(process.env.REFRESH_TOKEN_VALIDITY_DAYS || '0'),
-    issuer: process.env.TOKEN_ISSUER || '',
-    audience: process.env.TOKEN_AUDIENCE || '',
+    accessTokenValidityDays: parseInt(process.env.AUTH_ACCESS_TOKEN_VALIDITY_DAYS || '0'),
+    refreshTokenValidityDays: parseInt(process.env.AUTH_REFRESH_TOKEN_VALIDITY_DAYS || '0'),
+    issuer: process.env.AUTH_TOKEN_ISSUER || '',
+    audience: process.env.AUTH_TOKEN_AUDIENCE || '',
 };
 
 module.exports = tokenInfo
