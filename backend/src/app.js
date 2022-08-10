ed3c4d9d2094ecef7edc4cecc3b83753b2474f0e
erik santiago <blueowl2794@gmail.com>
2022-08-09 22:05:53 -0500

    add(modelsAndEndpoints)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,44 @@
 const express = require('express');
 // import route from './routes/index'
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+const cors = require('cors');
+
 const routes = require('./routes/index.js');
 
 const app = express();
 
-app.use(express.json({ limit: '50mb' }));
 
+app.use(cors());
+app.options('*', cors({
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}));
+
+require('./db.js'); 
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+}); 
 app.use("/api",routes)
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
 
-export default app;
+module.exports = app;
--- backend/src/controllers/services.js
+++ backend/src/controllers/services.js
@@ -1,16 +1,62 @@
-export const getServices = (req, res, next) =>{
-    res.send('getServices!');
+const { Servicio } = require('../db');
+
+export const getServices = async (req, res, next) =>{
+    const data = await Servicio.findAll();
+    if (data) {
+        try {
+            return res.json(data);
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        // console.log("No data found");
+        res.send("No service in db");
+    }
+    res.send(data);
+
+    // res.send('getServices!');
 }
 
-export const getService = (req, res, next) =>{
-    res.send('getService!');
+export const getService = async (req, res, next) =>{
+    const id = req.params.id;
+    if (!id) next({ msg: 'no mando id', status: 500 })
+
+    try {
+
+        const data = await Servicio.findAll({
+            where: { id: id },
+            attributes: ["id", "description"],
+        });
+
+        console.log("soy data ", data);
+
+        return res.json(data);
+    } catch (err) {
+        next(err);
+    }
+    // res.send('getService!');
 }
 export const getServicesCount = (req, res, next) =>{
     res.send('getServicesCount');
 }
 
-export const createServices = (req, res, next) =>{
-    res.send('createServices!');
+export const createServices = async (req, res, next) =>{
+    try {
+        const { name, value, date, time, description } = req.body;
+        const ServiceData = await Servicio.create({
+            name,
+            value,
+            date,
+            time,
+            description
+
+        })
+        return res.json(ServiceData);
+
+    } catch (err) {
+        next(err);
+    }
+    // res.send('createServices!');
 }
 
 
