f86ec54f43009da87444d90bc4329ea4af345abd
Joe <jb.barrientos@yahoo.com>
2022-08-09 21:32:05 -0300

    Refactor: Adding error catching to DB queries in login. Adding more message failure options and a consistent response from the authenticator

--- auth/routes/login/authenticate.js
+++ auth/routes/login/authenticate.js
@@ -5,10 +5,11 @@ function authenticate(validator, authenticator, tokenGenerator, authenticationEr
         const isValid = validator(user, password)
         if (!isValid) return res.status(400).send(authenticationError("Invalid username or password"))
 
-        const isAuthentic = await authenticator(user, password)
-        if (!isAuthentic) return res.status(403).send(authenticationError("Wrong username or password"))
-    
+        const {result, message} = await authenticator(user, password)
+        if (!result) return res.status(403).send(authenticationError(message))
+
         else res.send(tokenGenerator())
     }
 }
+
 module.exports = authenticate
