f8d8e7ea45b363542fadeea64335c6f08b97d542
Gabriel Chazarreta <gfchazarreta@gmail.com>
2022-08-09 21:00:36 -0300

    Adding folder and file globalStyles

--- frontend/quickly-app/src/components/views/supplier/HomeSupplier.jsx
+++ frontend/quickly-app/src/components/views/supplier/HomeSupplier.jsx
@@ -1,1 +1,11 @@
-// Busca
+import { Text, View } from "react-native";
+
+import {styles} from './styles'
+
+export default function HomeSupplier() {
+  return (
+    <View style={styles.container}>
+        <Text>Vista home - supplier</Text>
+    </View>
+  );
+}
