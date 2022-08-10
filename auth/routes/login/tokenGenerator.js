78fcb5bfea4e2fa74689f9075e2e9945129e0b70
Joe <jb.barrientos@yahoo.com>
2022-08-10 19:45:26 -0300

    Moved token generator function to utils

--- auth/routes/login/tokenGenerator.js
+++
@@ -1,9 +0,0 @@
-let createAccessToken = require('../../jwt/createAccessToken')
-let createRefreshToken = require('../../jwt/createRefreshToken')
-
-function tokenGenerator(user) {
-    let accessToken = createAccessToken(user)
-    let refreshToken = createRefreshToken(user)
-    return { accessToken, refreshToken }
-}
-module.exports = tokenGenerator
