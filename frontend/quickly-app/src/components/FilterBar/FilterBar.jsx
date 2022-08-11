80bf4c849d332ff2836a3dae1cbfaa86f293808f
Ivan <ivanbelasich@gmail.com>
2022-08-10 23:04:25 -0300

    test data and category picker

--- frontend/quickly-app/src/components/FilterBar/FilterBar.jsx
+++ frontend/quickly-app/src/components/FilterBar/FilterBar.jsx
@@ -1,8 +1,42 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+
+const categories = [
+  { key: 1, label: "Dermatología", value: "Dermatología" },
+  { key: 2, label: "Peluquería", value: "Peluquería" },
+  { key: 3, label: "Traumatología", value: "Traumatología" },
+];
+
+const usuarios = [
+  {
+    name: "Jorge",
+    category: "Dermatología",
+  },
+  {
+    name: "Raul",
+    category: "Peluquería",
+  },
+  {
+    name: "Luz",
+    category: "Traumatología",
+  },
+  {
+    name: "Martina",
+    category: "Peluquería",
+  },
+];
 
 const FilterBar = () => {
-  return <View>FilterBar</View>;
+  const [category, setCategories] = useState("");
+  const [data, setData] = useState([]);
+
+  return (
+    <View>
+      <RNPickerSelect
+        onValueChange={(value) => setCategories(value)}
+        items={categories}
+      />
+    </View>
+  );
 };
 
 export default FilterBar;
