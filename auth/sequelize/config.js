ce1784b7061fd482636cc51eeb63fa12b1412477
Joe <jb.barrientos@yahoo.com>
2022-08-14 19:17:06 -0300

    Adding necessary heroku pg config options

--- auth/sequelize/config.js
+++ auth/sequelize/config.js
@@ -8,9 +8,21 @@ module.exports = {
         host: process.env.AUTH_DB_HOST,
         port: process.env.AUTH_DB_PORT,
         dialect: 'postgres',
-        dialectOptions: {
-            bigNumberStrings: true
-        }
+
     },
+    production: {
+        username: process.env.AUTH_DB_USER,
+        password: process.env.AUTH_DB_PASSWORD,
+        database: process.env.AUTH_DB_NAME,
+        host: process.env.AUTH_DB_HOST,
+        port: process.env.AUTH_DB_PORT,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        },
+    }
 }
 
--- auth/sequelize/index.js
+++ auth/sequelize/index.js
@@ -1,13 +1,5 @@
 const { Sequelize } = require("sequelize")
-
-const sequelize = new Sequelize(
-    process.env.AUTH_DB_NAME, 
-    process.env.AUTH_DB_USER,
-    process.env.AUTH_DB_PASSWORD, {
-        host: process.env.AUTH_DB_HOST,
-        port: process.env.AUTH_DB_PORT,
-        dialect: "postgres"
-    } )
-
+const config = require('./config')
+const sequelize = new Sequelize(config[process.env.NODE_ENV])
 
 module.exports = sequelize
