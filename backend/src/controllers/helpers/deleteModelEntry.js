c7a5371841599eb1255be4e2e373d77402db7d1e
Joe <jb.barrientos@yahoo.com>
2022-08-17 19:58:22 -0300

    Bugfix: Removed console.log

--- backend/src/controllers/helpers/deleteModelEntry.js
+++ backend/src/controllers/helpers/deleteModelEntry.js
@@ -2,7 +2,6 @@ function deleteModelEntry(model) {
     return async function (props) {
         try {
             let rowsDestroyed = await model.destroy({ where: props })
-            console.log(rowsDestroyed)
             return rowsDestroyed > 0
         }
         catch (error) {
