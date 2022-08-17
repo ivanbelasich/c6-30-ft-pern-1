fbcdc395900467679c8ef37d6c895f4bfdbfb3da
Francisco Rey <reyf1998@outlook.es>
2022-08-17 19:34:25 -0300

    structure register and add imgs

--- frontend/quickly-app/src/components/views/auth/Register/Register.jsx
+++ frontend/quickly-app/src/components/views/auth/Register/Register.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { ScrollView, Text, View } from 'react-native';
+import {
+  ImageBackground,
+  ScrollView,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  TextInput,
+  TouchableHighlight,
+} from "react-native";
+
+import styles from "./styles";
+import { theme } from "../../../../globalStyles/theme";
 
 const Register = () => {
   return (
-    <ScrollView>
+    <ImageBackground
+      source={require("../../../../../assets/templates/TemplateRegister.png")}
+      resizeMode="cover"
+      style={styles.imageBackground}
+    >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.secondary}
+      />
       <View>
-          <Text>Registro</Text>
+        <Image
+          source={require("../../../../../assets/ClockRegister.png")}
+          style={styles.logo}
+          resizeMode="contain"
+        />
       </View>
-    </ScrollView>
-  )
-}
+      <View>
+        <Text>Nos encanta tenerte acá</Text>
+      </View>
+      <View>
+        <Text>Somos Quickly,</Text>
+        <Text>¿Cómo quieres que te llamemos?</Text>
+      </View>
+      <View>
+        <TextInput />
+      </View>
+      <View>
+        <TouchableHighlight>
+          <Text>Registrar</Text>
+        </TouchableHighlight>
+      </View>
+      <View>
+        <Text>¿Ya tienescuenta?</Text>
+        <Text>¡Inicia Sesión!</Text>
+      </View>
+    </ImageBackground>
+  );
+};
 
-export default Register
+export default Register;
--- frontend/quickly-app/src/components/views/auth/Register/styles.js
+++ frontend/quickly-app/src/components/views/auth/Register/styles.js
@@ -1,1 +1,14 @@
 // Register styles
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    flex: 1,
+  },
+  logo: {
+    width: "100%",
+    marginTop: 20,
+  },
+});
+
+export default styles;
