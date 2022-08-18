dfdf1f5c029176534f5f11a647c0c27eea4d16fc
Joe <jb.barrientos@yahoo.com>
2022-08-17 23:44:29 -0300

    Feat: working same user delete restriction

--- backend/src/routes/client.js
+++ backend/src/routes/client.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const router = Router();
 const Client = require('../sequelize/models/Client')
 const sanitizer = require('../middleware/sanitizer');
-const {  clientCreator, clientFindUser, clientDeleteUser } = require('../controllers/client/index')
+const { clientCreator, clientFindUser, clientDeleteUser } = require('../controllers/client/index')
+const jwtMiddleware = require('../middleware/jwt')
+const sameUserTokenAndBody = require('../middleware/sameUserTokenAndBody')
 
 let { id, createdAt, updatedAt, ...modelProps } = Client.getAttributes()
 let modelKeys = Object.keys(modelProps)
@@ -11,10 +13,11 @@ let sanitizeDelete = sanitizer(['user'])
 
 router.get('/', clientFindUser)
 router.post('/', sanitizePost, clientCreator)
-router.delete('/', sanitizeDelete, clientDeleteUser)
-
-
-//New Client
-
+router.delete('/',
+    sanitizeDelete,
+    jwtMiddleware,
+    sameUserTokenAndBody,
+    clientDeleteUser
+)
 
 module.exports = router;
