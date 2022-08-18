19c7cd2ca1cc543055cd530fa290a7b61cbecdfd
Joe <jb.barrientos@yahoo.com>
2022-08-17 21:38:38 -0300

    Feat: adding sanitizer middleware

--- auth/middleware/sanitizer.js
+++ auth/middleware/sanitizer.js
@@ -1,12 +1,10 @@
-let sanitizeError = require('./sanitizeError')
-
 let sanitizer = (fieldsArray) => {
     return function (req, res, next) {
         let body = {}
         for (let i = 0; i < fieldsArray.length; i++) {
             let field = fieldsArray[i]
             if (req.body[field] === undefined) {
-                return res.status(400).send(sanitizeError(`Property ${field} is missing or is undefined.`))
+                return res.status(400).send({ success: false, message: `Property ${field} is missing or is undefined.` })
             }
             else body[field] = req.body[field]
         }
