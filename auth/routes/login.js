67bddf1da3f95a5da888a9facb422091c969e293
Joe <jb.barrientos@yahoo.com>
2022-08-17 21:41:34 -0300

    "Feat: Login - Replacing validation function with middleware"

--- auth/routes/login.js
+++ auth/routes/login.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var authenticate = require('./login/authenticate');
 var loginAuthenticator = require('./login/loginAuthenticator');
-var loginValidator = require('./login/loginValidator');
 var createTokens = require('../jwt/createTokens');
 var authenticationError = require('./login/authenticationError')
 var sanitizer = require('../middleware/sanitizer')
 
 var router = express.Router();
 var sanitizeLogin = sanitizer(["user", "password"])
-var handlePost = authenticate(loginValidator, loginAuthenticator, createTokens, authenticationError)
+var handlePost = authenticate(loginAuthenticator, createTokens, authenticationError)
 
 router.post('/', sanitizeLogin, handlePost);
 
--- auth/routes/login/authenticate.js
+++ auth/routes/login/authenticate.js
@@ -1,10 +1,6 @@
-function authenticate(validator, authenticator, tokenGenerator, authenticationError) {
+function authenticate(authenticator, tokenGenerator, authenticationError) {
     return async function (req, res, next) {
         let { user, password } = req.body
-
-        const isValid = validator(user, password)
-        if (!isValid) return res.status(400).send(authenticationError("Invalid username or password"))
-
         const { result, message, payload } = await authenticator(user, password)
         
         if (!result.result) return res.status(403).send(authenticationError(result.message))
