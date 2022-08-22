f41de5fae492c605fba1215047058d5871e944b6
Joe <jb.barrientos@yahoo.com>
2022-08-22 16:29:08 -0300

    Bugfix: Removing unused lines

--- backend/src/routes/services.js
+++ backend/src/routes/services.js
@@ -1,16 +1,10 @@
 const { Router } = require('express');
 const { createService, deleteService, getOneService, getAllServices } = require('../controllers/services/index')
-// import { deleteServices, getService, getServices, getServicesCount, updateServices, saveServices, createServices } from '../controllers/services'
-
 const router = Router();
 
 router.get('/:id', getOneService);
 router.get('/', getAllServices)
-// router.get('/count',getServicesCount);
 router.post('/', createService);
-// router.get('/:id', getService);
-// router.post('/services', saveServices);
 router.delete('/', deleteService);
-// router.put('/:id', updateServices);
 
 module.exports = router;
