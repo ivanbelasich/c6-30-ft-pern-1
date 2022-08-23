60d10d57c6fc112e3d0551b5821ef69870c9f84b
Francisco Rey <reyf1998@outlook.es>
2022-08-23 12:46:43 -0300

    add styles

--- frontend/quickly-app/src/components/views/auth/ForgetPassword/ForgetPassword.jsx
+++ frontend/quickly-app/src/components/views/auth/ForgetPassword/ForgetPassword.jsx
@@ -1,11 +1,36 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import globalStyles from "../../../../globalStyles/globalStyles";
 
 const ForgetPassword = () => {
   return (
-    <View>
-      <Text>ForgetPassword</Text>
-    </View>
+    <ScrollView>
+      <MaterialCommunityIcons
+        name="lock-open-variant"
+        style={{ color: "#E2AB11", fontSize: 60 }}
+      />
+      <Text>
+        ¡Todo tiene solucion! vamos a enviarte un codigo para iniciar el
+        restablecimiento de la contraseña, por favor elige una de las opciones:
+      </Text>
+      <TouchableOpacity>
+        <View style={{ flexDirection: "row" }}>
+          <MaterialCommunityIcons
+            name="lock-open-variant"
+            style={{ color: "#E2AB11", fontSize: 60 }}
+          />
+          <View>
+            <Text>Via Email:</Text>
+            <Text>Lorem ipsum, dolor sit amet consectetur</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+      <View style={[globalStyles.normalButton]}>
+        <TouchableOpacity style={globalStyles.button}>
+          <Text style={globalStyles.textButton}>Continuar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
