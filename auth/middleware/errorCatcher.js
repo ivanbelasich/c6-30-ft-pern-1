6dfebc40c35c029f93850c19b124beccb29c2cc2
Joe <jb.barrientos@yahoo.com>
2022-08-26 03:53:17 -0300

    Feat: Adding failsafe error catching middleware

---
+++ auth/middleware/errorCatcher.js
@@ -0,0 +1,10 @@
+let errorResponse = require("../utils/errorResponse")
+
+function errorCatcher(error, req, res, next) {
+    console.log(error)
+    let { status, message } = error
+    if (status && message) return res.status(status).send(errorResponse(message))
+    else return res.status(500).send(errorResponse("Unexpected error"))
+}
+
+module.exports = errorCatcher
