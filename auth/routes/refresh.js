1773e9045bf4f1aca8bfd652755ce4f713736244
Joe <jb.barrientos@yahoo.com>
2022-08-26 07:11:27 -0300

    Bugfix: Fixed missing controller import

--- auth/routes/refresh.js
+++ auth/routes/refresh.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 let sanitizer = require('../middleware/sanitizer')
 let sanitizeRefresh = sanitizer(['refreshToken'])
+let { handlePost } = require('../controllers/refresh')
 
 router.post('/', sanitizeRefresh, handlePost);
 
