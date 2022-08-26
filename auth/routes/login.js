f60491adacdd3937c3f8f0b2f8319b8c6d7f0aa4
Joe <jb.barrientos@yahoo.com>
2022-08-26 04:11:40 -0300

    Refactor: split controller logic from route logic

--- auth/routes/login.js
+++ auth/routes/login.js
@@ -1,16 +1,11 @@
 var express = require('express');
-var authenticate = require('./login/authenticate');
-var loginAuthenticator = require('./login/loginAuthenticator');
-var createTokens = require('../jwt/createTokens');
-var sanitizer = require('../middleware/sanitizer');
-var findEntry = require('./login/findEntry');
-var payloadResponse = require('../utils/payloadResponse')
-
 var router = express.Router();
-var sanitizeLogin = sanitizer(["user", "password"])
-var handlePost = authenticate(findEntry, loginAuthenticator, createTokens, payloadResponse)
 
-router.post('/', sanitizeLogin, handlePost);
+var sanitizer = require('../middleware/sanitizer');
+var sanitizeLogin = sanitizer(["user", "password"])
+
+var { handlePostLogin } = require('../controllers/login')
+
+router.post('/', sanitizeLogin, handlePostLogin);
 
 module.exports = router;
-
