c7ef9fd95dea97f229727773dd1b0f65fc98091a
Joe <jb.barrientos@yahoo.com>
2022-08-26 04:33:25 -0300

    Refactor: split route and controllers for /available

--- auth/routes/available.js
+++ auth/routes/available.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var availabler = require('./available/availabler')
-var availableCheck = require('./available/availableCheck')
-var sanitizer = require('../middleware/sanitizer')
-
-var successResponse = require('../utils/successResponse');
-var sanitizeAvailable = sanitizer(['user'])
 var router = express.Router();
 
-var handlePost = availabler(availableCheck, successResponse)
+var sanitizer = require('../middleware/sanitizer')
+var sanitizeAvailable = sanitizer(['user'])
+
+var { handlePost } = require('../controllers/available')
 
 router.post('/', sanitizeAvailable, handlePost);
 
