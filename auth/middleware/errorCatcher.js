9bd9f9ace3961be98c4e47c1432da4a85eaf0f43
Joe <jb.barrientos@yahoo.com>
2022-08-26 04:40:06 -0300

    Feat: adding sequelize error handling, providing minimal information to the requester

--- auth/middleware/errorCatcher.js
+++ auth/middleware/errorCatcher.js
@@ -3,6 +3,7 @@ let errorResponse = require("../utils/errorResponse")
 function errorCatcher(error, req, res, next) {
     process.env.NODE_ENV === "development" && console.log(error)
     let { status, message } = error
+    if (error.name && error.parent) return res.status(400).send(errorResponse("User database error"))
     if (status && message) return res.status(status).send(errorResponse(message))
     else return res.status(500).send(errorResponse("Unexpected error"))
 }
