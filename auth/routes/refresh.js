94d9f9af01d995a280f8c5bb77e8dd702d5f1b11
Joe <jb.barrientos@yahoo.com>
2022-08-26 06:49:30 -0300

    Refactor: split /refresh controller and routes logic

--- auth/routes/refresh.js
+++ auth/routes/refresh.js
@@ -1,16 +1,6 @@
-var express = require('express');
-var router = express.Router();
-
-var refresher = require('./refresh/refresher')
-var getAccessToken = require('./refresh/getAccessToken')
-var tokenDecode = require('../jwt/tokenDecode')
-var tokenValidate = require('../jwt/tokenValidate')
-var findEntry = require('../controllers/login/findEntry')
-var payloadResponse = require('../utils/payloadResponse')
-var createTokens = require('../jwt/createTokens')
-var sanitizer = require('../middleware/sanitizer')
-
-let handlePost = refresher(getAccessToken, tokenDecode, tokenValidate, findEntry, createTokens, payloadResponse)
+let express = require('express');
+let router = express.Router();
+let sanitizer = require('../middleware/sanitizer')
 let sanitizeRefresh = sanitizer(['refreshToken'])
 
 router.post('/', sanitizeRefresh, handlePost);
