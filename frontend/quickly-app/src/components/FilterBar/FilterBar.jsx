e1df7626f538a6ef8e99cf6768d88fd7d8c2efaa
Ivan <ivanbelasich@gmail.com>
2022-08-30 19:20:57 -0300

    testing

--- frontend/quickly-app/src/components/FilterBar/FilterBar.jsx
+++ frontend/quickly-app/src/components/FilterBar/FilterBar.jsx
@@ -1,28 +1,58 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Alert } from "react-native";
-import Calendar from "../Calendar/Calendar";
+import { View, Text, Alert, Button } from "react-native";
+
 import axios from "axios";
 import globalStyles from "../../globalStyles/globalStyles";
 import { Picker } from "@react-native-picker/picker";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
-const FilterBar = ({ navigation }) => {
+const url = "https://quickly-a.herokuapp.com";
+
+const FilterBar = () => {
   const [category, setCategories] = useState("");
   const [provider, setProvider] = useState();
   const [usuarios, setUsuarios] = useState();
   const [data, setData] = useState();
-  const [prueba, setPrueba] = useState()
+  const [time, setTime] = useState();
+  const [date, setDate] = useState(new Date());
+  const [text, setText] = useState("Empty");
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const onChange = (e, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+
+    let tempDate = new Date(currentDate);
+    let fDate =
+      tempDate.getFullYear() +
+      "-" +
+      (tempDate.getMonth() + 1) +
+      "-" +
+      tempDate.getDate();
+    setText(fDate);
+    console.log(
+      new Date(text.concat("T" + time + ":00")).toLocaleString(),
+      "esta es la fecha"
+    );
+  };
 
   useEffect(() => {
-    axios.get("https://quickly-a.herokuapp.com/api/service").then((res) => {
+    axios.get(`${url}/api/service`).then((res) => {
       setData(res.data.payload.map((el) => el.category));
     });
   }, []);
 
   useEffect(() => {
-    axios.get("https://quickly-a.herokuapp.com/api/provider").then((res) => {
+    axios.get(`${url}/api/provider`).then((res) => {
       setUsuarios(res.data.payload);
     });
-    console.log(usuarios, "esto es usuarios");
   }, []);
 
   const categFiltered = data?.filter((item, index) => {
@@ -54,6 +84,20 @@ const FilterBar = ({ navigation }) => {
       }
     );
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const resp = await axios.post(`${url}/api/order`, {
+        client: "Ivan",
+        serviceId: provider,
+        date: new Date(text.concat("T" + time + ":00")).toLocaleString(),
+      });
+      console.log(resp.data, "esta es la datita");
+    } catch (error) {
+      console.log(error.resp, "este es el error");
+    }
+  };
+
   return (
     <View>
       <Picker
@@ -84,19 +128,40 @@ const FilterBar = ({ navigation }) => {
                 key={index}
                 value={el.Services[0].id}
               />
-            ) )
-           
-        ) }
+            ))
+        )}
       </Picker>
       <Text> id de proveedor: {provider}</Text>
-      <Text></Text>
-      <Text>Selecciona la fecha:</Text>
-      <Calendar />
+      <Button title="Seleccionar fecha" onPress={() => showMode("date")} />
+      <Text>Fecha elegida: {text}</Text>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          mode={mode}
+          is24Hour
+          display="default"
+          minimumDate={new Date(Date())}
+          value={date}
+          onChange={onChange}
+        />
+      )}
+      <Picker
+        selectedValue={time}
+        onValueChange={(itemValue) => setTime(itemValue)}
+      >
+        <Picker.Item label={"Selecciona un horario"} value={null} />
+        <Picker.Item label={"08:30"} value={"08:30"} />
+        <Picker.Item label={"09:30"} value={"09:30"} />
+        <Picker.Item label={"10:30"} value={"10:30"} />
+      </Picker>
+      <Text>Hora seleccionada: {time}</Text>
+      <Text>{text.concat("T" + time + ":00")}</Text>
       <View style={[globalStyles.button, globalStyles.normalButton]}>
         <Text onPress={showAlert} style={globalStyles.textButton}>
           + Guardar turno
         </Text>
       </View>
+      <Button onPress={handleSubmit} title="guarda pa" />
     </View>
   );
 };
