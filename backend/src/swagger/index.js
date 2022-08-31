f3af2251604bb68e46c312ab714c7a6f4c2d0b34
Joe <jb.barrientos@yahoo.com>
2022-08-31 03:38:08 -0300

    Bugfix: Moving swagger config file

--- backend/src/swagger/index.js
+++ backend/src/swagger/index.js
@@ -1,7 +1,6 @@
 let swaggerUI = require('swagger-ui-express')
 let yaml = require('yamljs')
-let path = require('path')
-let config = yaml.load(path.join(__dirname, 'swagger-config.yaml'))
+let config = yaml.load('./swagger-config.yaml')
 let express = require('express')
 let router = express.Router()
 router.use('/', swaggerUI.serve, swaggerUI.setup(config))
