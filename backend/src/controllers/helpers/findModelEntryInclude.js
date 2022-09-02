36741cac4e798b1492d01fd007014857067ebef0
Joe <jb.barrientos@yahoo.com>
2022-09-01 22:16:12 -0300

    Bugfix: adding necessary relationship between orders and sercices

--- backend/src/controllers/helpers/findModelEntryInclude.js
+++ backend/src/controllers/helpers/findModelEntryInclude.js
@@ -2,6 +2,7 @@ function findModelEntryInclude(model, modelInclude) {
     return async function (query) {
         try {
             let response = await model.findOne({ ...query, include: modelInclude })
+            console.log(response)
             return response
         }
         catch (error) {
