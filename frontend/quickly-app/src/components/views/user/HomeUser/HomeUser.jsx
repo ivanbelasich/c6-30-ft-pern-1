8f983a8e5b37a87f6545ea20c73b7a202b09beec
Ivan <ivanbelasich@gmail.com>
2022-09-03 15:12:30 -0300

    fin

--- frontend/quickly-app/src/components/views/user/HomeUser/HomeUser.jsx
+++ frontend/quickly-app/src/components/views/user/HomeUser/HomeUser.jsx
@@ -1,14 +1,41 @@
-import { StatusBar, Text, TouchableHighlight, View, } from "react-native";
+import { StatusBar, Text, TouchableHighlight, View } from "react-native";
 // Styles
 import globalStyles from "../../../../globalStyles/globalStyles";
 import { theme } from "../../../../globalStyles/theme";
+import { useCallback, useEffect, useState } from "react";
+import useUserData from "../../../../hooks/useUserData";
+import { useAuth } from "../../../../hooks/useAuth";
+import styles from "./styles";
 
 
-const HomeUser = ({navigation}) => {
+const HomeUser = ({ navigation }) => {
+  const { authData } = useAuth();
+  const { user, access } = authData;
+  const { userData, loading } = useUserData(user, access);
+
+  console.log(userData, " esto es la userData");
+
+  if (loading) return <View></View>;
   return (
     <View style={globalStyles.container}>
-      <StatusBar barStyle="light-content" backgroundColor={theme.colors.secondary}/>
-      <Text>Aquí debería haber una lista de turnos</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={theme.colors.secondary}
+      />
+      <Text>
+        {" "}
+        {userData?.Orders.length === 0 ? (
+          <Text>Todavia no tienes turnos agendados!</Text>
+        ) : (
+          userData?.Orders.map((k, index) => (
+            <View style={styles.container} key={`el${index}`}>
+              <Text>Proveedor:{k.provider}</Text>
+              <Text>Fecha:{new Date(k.date).toString()}</Text>
+              <Text>Descripción:{k.description}</Text>
+            </View>
+          ))
+        )}
+      </Text>
     </View>
   );
 };
